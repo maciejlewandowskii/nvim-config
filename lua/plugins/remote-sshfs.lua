b971c1b3005eda7fb33af994b7c1513459087a9c
maciejlewandowskii <maciej4612@gmail.com>
2024-08-23 18:14:44 +0200

    added remote sshfs for remote workflow

---
+++ lua/plugins/remote-sshfs.lua
@@ -0,0 +1,10 @@
+return {
+    "nosduco/remote-sshfs.nvim",
+    enabled = true,
+    opts = {
+        -- add any options here
+    },
+    dependencies = {
+        "nvim-telescope/telescope.nvim"
+    },
+}
